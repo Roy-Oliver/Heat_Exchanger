96e22eb08067ab4eae6710860b996ebc551e9407
oliver <oliverroymangosing1431@gmail.com>
2022-03-10 14:25:01 +0800

    coded log mean temperature difference function

---
+++ TCorrFactor.py
@@ -0,0 +1,30 @@
+def GetDTM(T1, T2, t1, t2, flow, shell_passes):
+    import math
+
+    if flow == "countercurrent":
+        DTLM = ((T1 - t2) - (T2 - t1)) / math.log((T1 - t2) / (T2 - t1))
+    else:
+        DTLM = ((T1 - t1) - (T2 - t2)) / math.log((T1 - t1) / (T2 - t2))
+
+    R = (T1 - T2) / (t2 - t1)
+    S = (t2 - t1) / (T1 - t1)
+
+    if shell_passes == 1:
+        # 1 shell pass and two or more even tube passes
+        numerator = math.sqrt((R ** 2) + 1) * math.log((1 - S) / (1 - R * S))
+        denom_1 = 2 - S * (R + 1 - math.sqrt((R ** 2) + 1))
+        denom_2 = 2 - S * (R + 1 + math.sqrt((R ** 2) + 1))
+        Ft = numerator / ((R - 1) * math.log(denom_1 / denom_2))
+
+    else:
+        # Equation: https://imgur.com/a/Q60laqU
+        # 2 shell passes and four or multiples of 4 tube passes
+        numerator = math.sqrt((R ** 2) + 1) * math.log((1 - S) / (1 - S * R))
+        denom_1 = (2 / S) - 1 - R + (2 / S) * math.sqrt((1 - S) * (1 - R * S)) + math.sqrt((R ** 2) + 1)
+        denom_2 = (2 / S) - 1 - R + (2 / S) * math.sqrt((1 - S) * (1 - R * S)) - math.sqrt((R ** 2) + 1)
+        Ft = numerator / (2 * (R - 1) * math.log(denom_1 / denom_2))
+
+    return DTLM * Ft
+
+
+
--- main.py
+++ main.py
@@ -2,47 +2,62 @@ from Tubes import Tubes
 from Shell import Shell
 from HeatExchanger import HeatExchanger
 from scipy import optimize
+from TCorrFactor import GetDTM
 
 def main():
     # Input Initial Guess for Overall Coefficient
-    U0ass = 500 # W/m2 K
+    U0ass = 600 # W/m2 K
+
+    # Input relative error for U0
+    relative_error = 0.30 # in decimal
+
+    # Input Flow Type - cocurrent or countercurrent
+    flow = "countercurrent"
 
     # Input duty
-    Q = 2280000 # W
+    Q = 4340000 # W
 
-    # Input mean temp difference
-    DTm = 48.6 # Kelvin
+    # Input properties of hot stream
+    T1 = 368.15 # K
+    T2 = 313.15 # K
+
+    # Input properties of cold stream
+    t1 = 298.15 # K
+    t2 = 313.15 # K
 
     # Input properties of TUBE Stream
-    m_tube_in = 27.27 # kg/s
-    Cp_tube = 4.18 * (10 ** 3) # J/kg K
-    mu_tube = 6.71 * (10 ** (-4)) # Pa s
-    rho_tube = 992.8 # kg/m3
-    k_tube = 0.631 # W/m K
+    m_tube_in = 68.9 # kg/s
+    Cp_tube = 4.2 * (10 ** 3) # J/kg K
+    mu_tube = 0.8 * (10 ** (-3)) # Pa s
+    rho_tube = 995 # kg/m3
+    k_tube = 0.59 # W/m K
 
     # Input properties of SHELL Stream
-    m_shell_in = 6.25 # kg/s
-    Cp_shell = 2.28 * (10 ** 3)  # J/kg K
-    mu_shell = 0.17 * (10 ** (-3))  # Pa s
-    rho_shell = 850  # kg/m3
-    k_shell = 0.125  # W/m K
+    m_shell_in = 27.778 # kg/s
+    Cp_shell = 2.84 * (10 ** 3)  # J/kg K
+    mu_shell = 0.34 * (10 ** (-3))  # Pa s
+    rho_shell = 750  # kg/m3
+    k_shell = 0.19  # W/m K
 
     # Input properties of TUBES
-    head_type = "PFH" # Options: PFH (pull-through floating head), SFH (split-ring floating head), OPB (outside packed bed), FAU (fixed and u-tube)
-    L = 4 # m
+    head_type = "SFH" # Options: PFH (pull-through floating head), SFH (split-ring floating head), OPB (outside packed bed), FAU (fixed and u-tube)
+    L = 4.83 # m
     di = 0.016 # m
     do = 0.020 # m
     pitch_type = "triangular" # "triangular" or "square"
-    tube_passes = 4
-    tube_fouling_factor = 1/0.00025 # W/m2 K
-    tube_thermal_resistance = 45 # W/m K
+    tube_passes = 2
+    tube_fouling_factor = 3000 # W/m2 K
+    tube_thermal_resistance = 50 # W/m K
 
     # Input properties of SHELL
     baffle_spacing = 0.2 # As a fraction of shell diameter
     baffle_cut = 25 # in percent. Available 15, 25, 35, 45,
-    shell_fouling_factor = 1/0.0002 # W/m2 K
-    shell_passes = 2
+    shell_fouling_factor = 5000 # W/m2 K
+    shell_passes = 1
 
+
+    # Compute for true temperature difference
+    DTm = GetDTM(T1, T2, t1, t2, flow, shell_passes)
 
     # Initialize Tubes Object
     he_tubes = Tubes(m_tube_in, Cp_tube, mu_tube, rho_tube, k_tube, head_type, L, di, do, pitch_type, tube_passes, tube_fouling_factor, tube_thermal_resistance)
@@ -66,10 +81,11 @@ def main():
         return err
 
     # Set the error to zero using newton's method
-    optimize.newton(error, U0ass)
+    optimize.newton(error, U0ass, rtol=relative_error)
 
     # Print properties
     print(f"U0 = {HE.U0}")
+    print(f"DTm = {DTm}")
     print("Tube Side:")
     print(f"   Tube Pressure Drop = {HE.tubes.deltaP}")
     print(f"   Velocity = {HE.tubes.velocity}\n")
